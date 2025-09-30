5ea0badaaf4f09d02b940b41e29934aa06211f03
ertipografo <mvtro.1@gmail.com>
2025-09-30 22:29:53 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+	return (
+		<div className="bg-red-400">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quo,
+			ducimus modi enim consequuntur qui nihil labore natus sapiente est
+			quod eum architecto ipsa, eveniet rerum. Quos cupiditate illo minus.
+		</div>
+	);
+}
