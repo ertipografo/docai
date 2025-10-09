fec2b2a490a5640e3bd2e5fe9bd23abe63ae3723
Alessandro Cavaterra <alessandro.c@docsity.com>
2025-10-09 17:35:20 +0200

    remexp

--- src/SidebarFeatures.jsx
+++ src/SidebarFeatures.jsx
@@ -32,52 +32,7 @@ export default function SidebarFeatures({ feature, setFeature }) {
               </div>
             </div>
             {isOpen && !isOriginal && (
-              <div className="gap-1 p-2 pt-0 bg-bg3 border-t border-bg1 pt-2 rounded-b flex justify-end items-center gap-2">
-                <label>Scegli formato:</label>
-                <div className="relative flex">
-                  <div className="w-14 bg-bg1 flex items-center pl-2 rounded-l font-semibold">
-                    {cur}
-                  </div>
-                  <div
-                    className="h-10 w-8 flexer bg-bg1 border-l border-borderColor rounded-r"
-                    onClick={() => setOpen((s) => !s)}
-                  >
-                    <ChevronDown size={16} />
-                  </div>
-                  {open && (
-                    <div className="p-2 bg-bg1 absolute top-full right-0 rounded-lg shadow-xl flex flex-col gap-1 mt-1">
-                      {featureModes.map((mode) => (
-                        <div
-                          className={`${
-                            mode === cur ? "bg-blue-50" : ""
-                          } justify-end gap-2 flex items-center p-1 rounded-md`}
-                          key={mode}
-                        >
-                          {mode === cur ? (
-                            <div className="flex-1 flexer text-blue-900">
-                              Free
-                            </div>
-                          ) : (
-                            <div className="bg-amber-100 text-amber-900 whitespace-nowrap text-xs px-2 py-1 rounded">
-                              Premium AI
-                            </div>
-                          )}
-
-                          <div
-                            className={`${
-                              cur === mode
-                                ? "bg-blue-500 text-white"
-                                : "bg-bg3 text-text2"
-                            } py-2 flexer min-w-12 rounded text-xs`}
-                          >
-                            {mode}
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
+              <div className="h-20 bg-bg3 flexer text-xs">feature premium</div>
             )}
           </div>
         );
