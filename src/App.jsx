a4d2bd5076f9998fb470a393e641859d249d33a8
Alessandro Cavaterra <alessandro.c@docsity.com>
2025-10-10 12:25:40 +0200

    megusta

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Sidebar from "./Sidebar";
 import OutBar from "./OutBar";
 import Logo from "./Logo";
 import { ChevronUp } from "lucide-react";
-import MobileFeatures from "./MobileFeatures";
+import FeatureTop from "./FeatureTop";
 
 export default function App() {
   const [feature, setFeature] = useState(features[0].value);
@@ -18,9 +18,6 @@ export default function App() {
         <Logo className="scale-75" />
       </div>
       <div className="lg:flex-1 lg:flex lg:overflow-hidden">
-        {/* <div className="w-headerHeight hidden lg:block bg-bg3 relative z-50">
-          <OutBar show={show} setShow={setShow} />
-        </div> */}
         <div className="lg:flex lg:flex-row flex-1 lg:overflow-auto">
           <div
             className={`${
@@ -35,22 +32,12 @@ export default function App() {
               setShow={setShow}
             />
           </div>
-          <MobileTitle />
           <div className="bg-bg3 flex-1 flex flex-col lg:overflow-auto px-3 pb-20">
-            <MobileFeatures />
+            <FeatureTop feature={feature} setFeature={setFeature} />
 
-            <div className="sticky top-headerHeight top-headerHeight3 lg:top-0 z-50 bg-bg3  pt-0 lg:pt-3">
-              <div className="max-w-documentWidth mx-auto">
-                <div className="pt-3 lg:pt-0">
-                  <div className="p-4 bg-bg1 h-headerHeight rounded-t flex-1">
-                    13
-                  </div>
-                </div>
-              </div>
-            </div>
             <div className="flex-1 overflow-visible flex">
-              <div className="bg-red-300 flex-1 max-w-documentWidth mx-auto">
-                <div className="h-[5000px] bg-rose-100"></div>
+              <div className="flex-1">
+                <div className="h-[5000px] bg-bg1"></div>
               </div>
             </div>
           </div>
---
+++ src/FeatureTop.jsx
@@ -0,0 +1,57 @@
+import { ChevronDown, Settings, Settings2 } from "lucide-react";
+import { features } from "./utils";
+const MobileFeatures = ({ feature, setFeature }) => {
+  return (
+    <div className="mb-3 h-headerHeight lg:hidden flex items-center gap-2">
+      {features.map((f) => {
+        const { Icon } = f;
+        return (
+          <div
+            key={f.value}
+            onClick={() => setFeature(feature === f.value ? null : f.value)}
+            className={`${
+              feature === f.value ? "bg-violet-500 text-textOnDark" : "bg-bg1"
+            } flex-1 px-2 flex rounded justify-center cursor-pointer gap-2 items-center h-full`}
+          >
+            <Icon size={18} />
+            <span className="font-semibold capitalize hidden sm:flex">
+              {f.value}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default function FeatureTop({ feature, setFeature }) {
+  const currentFeature = features.find((f) => f.value === feature);
+
+  const featureLabel = currentFeature?.label ?? "niente";
+  const FeatureIcon = currentFeature?.Icon;
+  const hasComplementary = currentFeature?.hasComplementary;
+  return (
+    <div className="sticky bg-bg3 top-headerHeight lg:top-0 pt-3 top-headerHeight border-b border-borderColor">
+      <MobileFeatures feature={feature} setFeature={setFeature} />
+      <div className="bg-bg1 rounded-t flex items-center px-2 h-headerHeight">
+        <div className="flex-1 flex items-center justify-start">
+          {hasComplementary && (
+            <div className="bg-actionSecondary rounded cursor-pointer h-buttonHeight aspect-square flexer">
+              <Settings2 size={16} />
+            </div>
+          )}
+        </div>
+        <div className="flex-1 flex items-center justify-center gap-2 font-semibold hidden sm:flex">
+          {FeatureIcon && <FeatureIcon size={18} />}
+          {featureLabel}
+        </div>
+        <div className="flex-1 flex items-center justify-end">
+          <div className="flexer gap-2 h-buttonHeight px-3 text-xs font-semibold bg-actionSecondary rounded cursor-pointer">
+            <span>Doload</span>
+            <ChevronDown size={16} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
