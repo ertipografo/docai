fb7f61d2790dfedf5b96aa0f4d16878ab2dbdf04
Alessandro Cavaterra <alessandro.c@docsity.com>
2025-10-12 11:29:01 +0200

    pfsc

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Toolbar from "./Toolbar";
 import Header from "./Header";
 import Feature from "./Feature";
 import Chat from "./Chat";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 export default function App() {
   const [feature, setFeature] = useState(features[0].value);
@@ -34,27 +36,32 @@ export default function App() {
             />
           </div>
           <div className="bg-bg2 flex-1 flex flex-col lg:overflow-auto">
-            <div
-              className={`pl-3 pr-3 lg:pl-0 ${
-                feature !== "mappa" && "max-w-documentWidth"
-              } mx-auto w-full`}
-            >
-              <FeatureTop
-                feature={feature}
-                setFeature={setFeature}
-                showFeatureBar={showFeatureBar}
-                setShowFeatureBar={setShowFeatureBar}
-              />
-              <div className="flex-1 flex flex">
-                <Toolbar
+            <PerfectScrollbar className="overflow-visible! lg:overflow-hidden!">
+              <div
+                className={`pl-3 pr-3 lg:pl-0 ${
+                  feature !== "mappa" && "max-w-documentWidth"
+                } mx-auto w-full`}
+              >
+                <FeatureTop
                   feature={feature}
+                  setFeature={setFeature}
                   showFeatureBar={showFeatureBar}
                   setShowFeatureBar={setShowFeatureBar}
                 />
-                <Feature feature={feature} />
+                <div className="flex-1 flex">
+                  <Toolbar
+                    feature={feature}
+                    showFeatureBar={showFeatureBar}
+                    setShowFeatureBar={setShowFeatureBar}
+                  />
+                  <Feature feature={feature} />
+                </div>
               </div>
-            </div>
-            <BottomBar />
+              <BottomBar />
+              <div className="min-h-20 flexer my-3 mr-3 text-text2">
+                Footerino
+              </div>
+            </PerfectScrollbar>
           </div>
           <Chat showChat={showChat} setShowChat={setShowChat} />
         </div>
