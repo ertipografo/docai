2805cefb44ca5c77f038a3c96ce0232dde6ddd2c
Alessandro Cavaterra <alessandro.c@docsity.com>
2025-10-20 10:28:16 +0200

    ok

--- src/SuggestedDocs.jsx
+++ src/SuggestedDocs.jsx
@@ -1,18 +1,76 @@
-import { Star, FileText, Calendar, ChevronDown } from "lucide-react";
-import { features, getRandomArbitrary, titles } from "./utils";
+import { categories, getRandomArbitrary, titles, features } from "./utils";
 import { useState } from "react";
-
-const categories = [
-  { label: "preferiti", Icon: Star },
-  { label: "visti di recente", Icon: Calendar },
-  { label: "suggeriti", Icon: FileText },
-];
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 export default function SuggestedDocs() {
   const [cur, setCur] = useState(categories[0].label);
   const [show, setShow] = useState(true);
 
   return (
+    <div className="flex-1 hidden lg:flex flex-col overflow-hidden mt-auto">
+      <div className="flex-1 overflow-auto">
+        <PerfectScrollbar>
+          <div className="flex flex-col px-padding-lg">
+            <span className="z-30 bg-bg1 sticky top-0 h-btn flex items-center font-semibold text-xs text-text2">{`Documenti ${cur}`}</span>
+            {Array.from({ length: 10 }).map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className="rounded-btn bg-bg2/50 hover:bg-bg2 cursor-pointer p-padding-sm mb-padding-xs text-xs flex flex-col gap-padding-sm"
+                >
+                  <div>{titles[getRandomArbitrary(0, titles.length - 1)]}</div>
+                  <div className="flex items-center gap-padding-sm justify-between">
+                    <div className="flex leading-1 text-text2 items-center gap-padding-sm">
+                      23/5/2025
+                    </div>
+                    <div className="flex items-center gap-padding-sm">
+                      {features.map((f, i) => {
+                        const { value, Icon } = f;
+                        return (
+                          <div
+                            key={value}
+                            className={`${
+                              i <= getRandomArbitrary(0, 4)
+                                ? "opacity-60"
+                                : "opacity-20"
+                            }`}
+                          >
+                            <Icon size={14} />
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </PerfectScrollbar>
+      </div>
+      <div className="h-header flexer gap-padding-sm">
+        {categories.map((r) => {
+          return (
+            <div
+              key={r.label}
+              onClick={() => {
+                setCur(r.label);
+                setShow(true);
+              }}
+              className={`${
+                show && cur === r.label
+                  ? "bg-action-secondary"
+                  : "text-text2 hover:text-text1 hover:bg-action-secondary"
+              } h-btn w-btn flexer rounded-btn cursor-pointer`}
+            >
+              <r.Icon size={14} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+
+  /*  return (
     <div className="hidden lg:flex flex-col overflow-hidden mt-auto border-t border-border px-padding-lg">
       <div className="flex flex-col">
         {show && (
@@ -87,5 +145,5 @@ export default function SuggestedDocs() {
         })}
       </div>
     </div>
-  );
+  ); */
 }
