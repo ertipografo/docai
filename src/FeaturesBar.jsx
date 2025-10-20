41f3ac7975bb023b66263d0ba8ae11d22f633279
Alessandro Cavaterra <alessandro.c@docsity.com>
2025-10-20 10:27:06 +0200

    ok

--- src/FeaturesBar.jsx
+++ src/FeaturesBar.jsx
@@ -19,30 +19,31 @@ export default function FeaturesBar({
         {features.map((f, i) => {
           const isCur = feature === f.value;
           const { Icon } = f;
-          return noteType === "note" && i === 0 ? (
-            <></>
-          ) : (
-            <div
-              key={i}
-              onClick={() => {
-                setFeature(f.value);
-              }}
-              className={`${btn} ${isCur ? active : inactive}`}
-            >
-              <div className={inner}>
-                <Icon className="hidden sm:flex" size={16} />
-                <span>{f.value}</span>
-              </div>
-              {isCur ? (
-                isLoading ? (
-                  <Loader size={16} className="ml-auto animate-spin" />
+          return (
+            noteType === "note" &&
+            i !== 0 && (
+              <div
+                key={i}
+                onClick={() => {
+                  setFeature(f.value);
+                }}
+                className={`${btn} ${isCur ? active : inactive}`}
+              >
+                <div className={inner}>
+                  <Icon className="hidden sm:flex" size={16} />
+                  <span>{f.value}</span>
+                </div>
+                {isCur ? (
+                  isLoading ? (
+                    <Loader size={16} className="ml-auto animate-spin" />
+                  ) : (
+                    <ChevronDown size={16} className="hidden sm:flex ml-auto" />
+                  )
                 ) : (
-                  <ChevronDown size={16} className="hidden sm:flex ml-auto" />
-                )
-              ) : (
-                <ChevronRight size={16} className="hidden sm:flex ml-auto" />
-              )}
-            </div>
+                  <ChevronRight size={16} className="hidden sm:flex ml-auto" />
+                )}
+              </div>
+            )
           );
         })}
       </div>
