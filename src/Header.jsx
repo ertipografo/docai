260ee0aa2b991ef4e87f55cd432f8e6b69e0dd5c
Alessandro Cavaterra <alessandro.c@docsity.com>
2025-10-21 09:53:50 +0200

    ok

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,9 @@
 import Logo from "./Logo";
-import UploadCta from "./UploadCta";
 export default function Header() {
   return (
     <div className="px-padding-sm lg:px-padding-lg py-padding-sm flex items-center justify-between">
       <div className="h-btn-lg items-center flex">
         <Logo />
-      </div>
-      <div className="flex lg:hidden">
-        <UploadCta />
       </div>
     </div>
   );
